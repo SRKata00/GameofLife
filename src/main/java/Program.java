c3ae35971bb317e23d640c6a2ee4f3f240cf722f
Sandor-Rokaly Kata <2000srkata@gmail.com>
2021-11-22 15:07:06 +0200

    Fix variables

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,9 +1,14 @@
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Program {
+    static ThreadLocalRandom random;
+    static int nutritions=10;
+    static Object nutritionLock = new Object();
+
     public static void main (String[] args)
     {
-        int nutritions;
         ConcurrentLinkedQueue<Cell> cells = new ConcurrentLinkedQueue<Cell>();
         for (int i = 0; i < 3; i++) {
             cells.add(new SexualCell());
