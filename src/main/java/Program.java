442e851460a5fe488da9d4d511d6763f437026f7
Sandor-Rokaly Kata <2000srkata@gmail.com>
2021-11-23 01:37:27 +0200

    Add stopwatch and write Program.main()

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,11 +1,14 @@
+import org.hsqldb.lib.StopWatch;
+
+import java.util.ArrayList;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Program {
     static ThreadLocalRandom random;
     static int nutritions=10;
     static Object nutritionLock = new Object();
+    static StopWatch stopWatch = new StopWatch();
 
     public static void main (String[] args)
     {
@@ -14,5 +17,38 @@ public class Program {
             cells.add(new SexualCell());
             cells.add(new AsexualCell());
         }
+        ArrayList<Thread> threadList = new ArrayList<>();
+        for (Cell c : cells)
+        {
+            threadList.add(new Thread(){public void run(){ c.Live(); } });
+        }
+        threadList.add(new Thread(){public void run()
+        {
+            stopWatch.start();
+            write("Time: "+stopWatch.elapsedTime(),true);
+            write("\nCells",false);
+            for (Cell c: cells){write(c.toString()+"                         ",false);}
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } });
+        for (Thread t:threadList)
+        {
+            t.start();
+        }
+        stopWatch.stop();
+    }
+
+    public static synchronized void write(String text, boolean b)
+    {
+        if(b) {
+            char escCode=0x18;
+            int row=0;
+            int column =0;
+            System.out.print(String.format("%c[%d;%df]", escCode, row,column));
+        }
+        System.out.println(text);
     }
 }
