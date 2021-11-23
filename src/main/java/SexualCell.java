624e9f9c76c6f8c5ffcff09ec978058d3c3bb67c
Sandor-Rokaly Kata <2000srkata@gmail.com>
2021-11-23 03:10:53 +0200

    make TODO List for the further work

--- src/main/java/SexualCell.java
+++ src/main/java/SexualCell.java
@@ -10,7 +10,30 @@ public class SexualCell extends Cell{
 
     @Override
     public void Divide() {
-
+        if (CanDivide())
+        {
+            //TODO make dividing - send sign for a sexual cell who wants to divide
+        }
+        else
+        {
+            boolean b=false;
+            for(Cell c:Program.cells) //TODO: can be optimized
+            {
+                if ((c!=this)&&(c instanceof SexualCell) && (c.state!=cellState.Dead))
+                {
+                    b=true;
+                }
+            }
+            if(b)
+            {
+                //TODO wait for sign
+            }
+            else
+            {
+                state=cellState.Ok;
+            }
+            Program.sCellToDivide.add(this);
+        }
     }
 
     @Override
@@ -18,10 +41,7 @@ public class SexualCell extends Cell{
         boolean b=false;
         if (Program.sCellToDivide.isEmpty())
         {
-            for(Cell c:Program.cells) //TODO: can be optimized
-            {
-                b=((c instanceof SexualCell) && (c.state!=cellState.Dead));
-            }
+            //TODO choose a cell and check if this is in the ArrayList
         }
         else
         {
